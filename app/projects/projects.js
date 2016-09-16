ac1c6c821f4f7562daab0f55ba2f89d40d695b17
Tra Nguyen <tra.nguyen@2359media.com>
2016-09-16 14:50:35 +0800

    Implement projects using sortable view

---
+++ app/projects/projects.js
@@ -0,0 +1,19 @@
+'use strict';
+
+angular.module('myApp.projects', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/projects', {
+    templateUrl: 'projects/projects.html',
+    controller: 'ProjectsCtrl'
+  });
+}])
+
+.controller('ProjectsCtrl', ['$scope', function($scope) {
+	$scope.projects = {};
+	$scope.projects.todo = ["A","B"];
+	$scope.projects.progress = ["C","D"];
+	$scope.projects.done = ["3.0", "3.1", "3.2", "3.3", "3.4", "3.5"];
+
+
+}]);
